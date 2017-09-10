cbaec2ee7e0f8255b77cb771912574308d2e656e
Mario Juarez <mario@mjp.one>
2017-09-10 16:42:58 +0200

    Add shell script to run all benchmarks

---
+++ run-benchmarks.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+crystal build crystal/entire-file.cr --release -o crystal/bin/entire-file
+crystal build crystal/line-per-line.cr --release -o crystal/bin/line-per-line
+
+bench 'crystal/bin/entire-file input-text.txt'
+bench 'crystal/bin/line-per-line input-text.txt'
+
+bench 'node javascript/entire-file.js input-text.txt'
+bench 'node javascript/line-per-line.js input-text.txt'
+
+bench 'php php/entire-file.php input-text.txt'
+bench 'php php/line-per-line.php input-text.txt'
