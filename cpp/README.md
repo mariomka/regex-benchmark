f001bf0deb7184393002d5ce8d0048c7b73e9c3e
E Kawashima <e-kwsm@users.noreply.github.com>
2018-07-28 02:43:26 +0900

    Implement C++ Benchmark

---
+++ cpp/README.md
@@ -0,0 +1,13 @@
+# C++ Benchmark
+
+## How to build
+
+```sh
+g++ -std=c++11 -O3 benchmark.cpp -o bin/benchmark
+```
+
+## How to run
+
+```sh
+./benchmark <filename>
+```
---
+++ cpp/benchmark.cpp
@@ -0,0 +1,45 @@
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <regex>
+
+void measure(const std::string& data, const std::string& pattern) {
+  using clock = std::chrono::high_resolution_clock;
+  const auto start = clock::now();
+
+  const std::regex re{pattern};
+  unsigned count = 0;
+
+  for (std::sregex_token_iterator it{data.cbegin(), data.cend(), re}, end{}; it != end; ++it)
+    count++;
+
+  const auto end = clock::now();
+  const double elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() * 1e-6;
+  std::cout << elapsed << " - " << count << "\n";
+}
+
+int main(int argc, char** argv) {
+  if (argc != 2) {
+    std::cerr << "Usage: benchmark <filename>\n";
+    return 1;
+  }
+
+  std::ifstream file{argv[1]};
+  if (!file) {
+    std::cerr << "unable to open " << argv[1] << "\n";
+    return 1;
+  }
+
+  const std::string data{std::istreambuf_iterator<char>{file}, std::istreambuf_iterator<char>{}};
+
+  // Email
+  measure(data, "[\\w.+-]+@[\\w.-]+\\.[\\w.-]+");
+
+  // URI
+  measure(data, "[\\w]+:\\/\\/[^\\/\\s?#]+[^\\s?#]+(?:\\?[^\\s#]*)?(?:#[^\\s]*)?");
+
+  // IP
+  measure(data, "(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9])\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9])");
+
+  return 0;
+}
