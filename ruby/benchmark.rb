b5126cc37b501ef8170ff5634f8247219237815e
Mario Juarez <mario@mjp.one>
2019-11-13 19:20:45 +0100

    Ruby: Include regex compilation in measuring

--- ruby/benchmark.rb
+++ ruby/benchmark.rb
@@ -8,7 +8,7 @@ end
 def measure(data, pattern)
   count = 0
   elapsed = Benchmark.measure {
-    count = data.scan(pattern).size
+    count = data.scan(Regexp.compile(pattern)).size
   }
 
   puts "#{elapsed.real * 1e3} - #{count}"
@@ -17,10 +17,10 @@ end
 data = File.read(ARGV[0])
 
 # Email
-measure(data, /[\w\.+-]+@[\w\.-]+\.[\w\.-]+/)
+measure(data, '[\w\.+-]+@[\w\.-]+\.[\w\.-]+')
 
 # URI
-measure(data, /[\w]+:\/\/[^\/\s?#]+[^\s?#]+(?:\?[^\s#]*)?(?:#[^\s]*)?/)
+measure(data, '[\w]+:\/\/[^\/\s?#]+[^\s?#]+(?:\?[^\s#]*)?(?:#[^\s]*)?')
 
 # IP
-measure(data, /(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9])/)
+measure(data, '(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9])')
