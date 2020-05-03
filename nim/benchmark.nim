71fd001e1d2b64a96ef333676f5f1d24ba919554
özgür altunyurt <altunyurt@gmail.com>
2020-05-03 12:40:31 +0300

    Moved pattern compilation into measure function

--- nim/benchmark.nim
+++ nim/benchmark.nim
@@ -7,16 +7,17 @@ if paramCount() == 0:
   echo "Usage: ./benchmark <filename>"
   quit(QuitFailure)
 
-proc measure(data:string, pattern:Regex) =
+proc measure(data:string, pattern:string) =
   let time = cpuTime()
-  let matches: seq[string] = data.findAll(pattern)
+  let r_pattern = re(pattern)
+  let matches: seq[string] = data.findAll(r_pattern)
   let elapsed_time = cpuTime() - time 
   echo &"{elapsed_time * 1e3} - {len(matches)}"
 
 let data = readFile(paramStr(1))
 
-measure(data, re"[\w\.+-]+@[\w\.-]+\.[\w\.-]+")
+measure(data, r"[\w\.+-]+@[\w\.-]+\.[\w\.-]+")
 
-measure(data, re"[\w]+://[^/\s?#]+[^\s?#]+(?:\?[^\s#]*)?(?:#[^\s]*)?")
+measure(data, r"[\w]+://[^/\s?#]+[^\s?#]+(?:\?[^\s#]*)?(?:#[^\s]*)?")
 
-measure(data, re"(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9])")
+measure(data, r"(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9])")
